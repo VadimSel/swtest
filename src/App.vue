b9320a73de326871e9a87ee2178116f1e8d71a33
VadimSel <selyaaa33@gmail.com>
2025-01-27 11:37:41 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <MyHello title="Кросовки Puma" :price="9500" :isAdded="true" />
+  <!-- <MyHello is-added />
+  <MyHello /> -->
+</template>
+
+<style scoped></style>
